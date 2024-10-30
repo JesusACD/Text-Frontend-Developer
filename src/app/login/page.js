26c2048a3a1dd24ef0d304466141f0642a0823c3
JesusACD <jacarranza404@gmail.com>
2024-10-30 03:44:25 -0500

    e ha añadido la validación básica para el formulario de inicio de sesión

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,44 +1,57 @@
 "use client";
 
+import React, { useState } from "react";
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
   const handleSubmit = (event) => {
     event.preventDefault();
-    window.location.href = "/dashboard/sales-form";
+
+    if (username === "admin" && password === "admin") {
+      window.location.href = "/dashboard/sales-form";
+    } else {
+      setError("Invalid credentials. Please try again.");
+    }
   };
   return (
     <div className="h-screen flex justify-center items-center bg-white">
       <div className="max-w-md w-full bg-background p-10 rounded-none shadow-none">
-        <h2 className="text-spaceCadet text-3xl mb-5 font-bold">
-          Iniciar Sesión
-        </h2>
+        <h2 className="text-spaceCadet text-3xl mb-5 font-bold">Login</h2>
         <form onSubmit={handleSubmit}>
           <label
             className="block text-spaceCadet text-lg mb-2"
             htmlFor="username"
           >
-            Usuario
+            Username
           </label>
           <input
             id="username"
             type="text"
             className="w-full p-3 text-sm text-spaceCadet bg-white outline-none border-none focus:border-transparent focus:ring-0 mb-5"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
           />
           <label
             className="block text-spaceCadet text-lg mb-2"
             htmlFor="password"
           >
-            Contraseña
+            Password
           </label>
           <input
             id="password"
             type="password"
             className="w-full p-3 text-sm text-spaceCadet bg-white outline-none border-none focus:border-transparent focus:ring-0 mb-5"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
           />
+          {error && <p className="text-red-600 text-sm mb-5">{error}</p>}
           <button
             className="bg-backgroundBlue text-white p-3 w-full text-lg hover:bg-blue-600"
             type="submit"
           >
-            Iniciar Sesión
+            Login
           </button>
         </form>
       </div>
