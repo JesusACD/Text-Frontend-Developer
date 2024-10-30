cbdc8719a8c7bb7fdbc52e912ab1901680c7e281
JesusACD <jacarranza404@gmail.com>
2024-10-30 03:55:29 -0500

    feat: agrego botón que redirige a vista detallada de ventas en sales-details

--- src/app/dashboard/sales/page.js
+++ src/app/dashboard/sales/page.js
@@ -1,10 +1,11 @@
 "use client";
 import useOrderCRUD from "@/app/store/store";
 import Head from "@/components/Head/Head";
+import Link from "next/link";
 
 const Sales = () => {
   const { orders } = useOrderCRUD();
-  console.log("ordersPAGES", orders);
+  console.log("orders", orders);
 
   return (
     <>
@@ -24,7 +25,7 @@ const Sales = () => {
               }
 
               return (
-                <li key={sale.id} className="py-4">
+                <li key={`${sale.id}-${index}`} className="py-4">
                   <div className="flex justify-between">
                     <h3 className="text-spaceCadet text-xl font-bold">
                       {sale.client}
@@ -40,14 +41,14 @@ const Sales = () => {
                     </p>
                   </div>
                   <h4 className="text-spaceCadet text-lg font-bold mb-2">
-                    Detalles de la venta
+                    Sale Details
                   </h4>
                   <table className="w-full">
                     <thead>
                       <tr>
-                        <th className="text-spaceCadet text-sm">Nombre</th>
-                        <th className="text-spaceCadet text-sm">Cantidad</th>
-                        <th className="text-spaceCadet text-sm">Precio</th>
+                        <th className="text-spaceCadet text-sm">Name</th>
+                        <th className="text-spaceCadet text-sm">Quantity</th>
+                        <th className="text-spaceCadet text-sm">Price</th>
                         <th className="text-spaceCadet text-sm">Subtotal</th>
                       </tr>
                     </thead>
@@ -70,6 +71,17 @@ const Sales = () => {
                       ))}
                     </tbody>
                   </table>
+                  <div className="flex justify-between mt-6 text-lg font-semibold text-spaceCadet">
+                    <Link
+                      href={`/dashboard/sales-details/${sale.id}`}
+                      legacyBehavior
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <a className="mt-4 px-4 py-2 bg-[var(--backgroundBlue)] text-white">
+                        More Details
+                      </a>
+                    </Link>
+                  </div>
                 </li>
               );
             })}
